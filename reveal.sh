17e337a24f18abc630c19d7293ea4fd3a4d65699
Steven Kalinke <steven.kalinke@gmail.com>
2021-01-06 20:00:44 +0100

    Add User Input To Dry-Run Without Copying

--- reveal.sh
+++ reveal.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-echo "+++++ Font Revealer for Adobe Creative Cloud +++++"
+echo "❦ Font Revealer for Adobe Creative Cloud ❦"
 
-
-
-# while true; do
-#     read -p "Dry run?" yn
-#     case $yn in
-#         [Yy]* ) make install; break;;
-#         [Nn]* ) exit;;
-#         * ) echo "Please answer yes or no.";;
-#     esac
-# done
+echo "Dry-Run will preview the renaming process without copy any files."
+select dce in "Dry-Run" "Copy" "Exit"; do
+    case $dce in
+        Dry-Run ) isDry=true; echo "Dry-running..."; break;;
+        Copy ) isDry=false; echo "Copy & rename files..."; break;;
+        Exit ) exit;;
+    esac
+done
 
 cd $HOME/Library/Application\ Support/Adobe/CoreSync/plugins/livetype/.r/
 
@@ -20,18 +18,16 @@ for file in .*.otf
 do  
   # the "Postscript name:" does not contain spaces. good for file names.
   fontName=`otfinfo --info ${file} | fgrep "PostScript name:" | grep -oE "[^ ]+$"`
-  # create a FONT dir within downloads, if there is none
-  mkdir -p $HOME/Downloads/FONTS/
-  # copy files
-  cp $file "$HOME/Downloads/FONTS/$fontName.otf"
+  
+  if [ "$isDry" = false ] ; then
+    # create a FONT dir within downloads, if there is none
+    mkdir -p $HOME/Downloads/FONTS/
+    # copy files (if no dry-run)
+    cp $file "$HOME/Downloads/FONTS/$fontName.otf"
+  fi
+
   # echo changes:
   echo -e "$file\t⮕\t$fontName.otf"
 done
 
-
-
-
 echo "🚀 DONE! Check your 'FONTS' directory within Downloads"
-
-
-
